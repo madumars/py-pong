a6108c7c1b4cb215b91fef91ae541605b6a8f43a
madumars <mariaeduardam08@gmail.com>
2025-10-13 22:14:26 -0300

    colisão objetos

--- pong.py
+++ pong.py
@@ -52,19 +52,85 @@ game_rules = {
     "ball_speed": 5
 }
 
-time.sleep(10)
+#Painel pontuação (arrumar estilo depois)
+score_display = turtle.Turtle()
+score_display.penup()
+score_display.hideturtle()
+score_display.goto(0, 200)
+score_display.write("Jogador 1: 0 Jogador 2: 0", align="center", font=("Arial", 20, "normal"))
 
+#movimento da bola
+
+def hurdle_left_up():
+    y = hurdle_left.ycor()
+    if y < 250:
+        y += 20
+        hurdle_left.sety(y)
+        
+def hurdle_left_down():
+    y = hurdle_left.ycor()
+    if y > -240:
+        y -= 20
+        hurdle_right.sety(y)
+        
+def hurdle_right_up():
+    y = hurdle_right.ycor()
+    if y < 250:
+        y += 20
+        hurdle_right.sety(y)
+        
+def hurdle_right_down():
+    y = hurdle_right.ycor()
+    if y > -240:
+        y -= 20
+        hurdle_right.sety(y)
+        
 #Teclas
-#sc.listen()
-#sc.onkeypress(hurdleupA, "w")  
-#sc.onkeypress(hurdledownA, "s") 
-#sc.onkeypress(hurdlebupB, "Up")
-#sc.onkeypress(hurdledownB, "Down")
+sc.listen()
+sc.onkeypress(hurdle_left_up, "w")  
+sc.onkeypress(hurdle_left_down, "s") 
+sc.onkeypress(hurdle_right_up, "Up")
+sc.onkeypress(hurdle_right_down, "Down")
+
+#ball.setx(ball.xcor() + ball.dx)
+#ball.sety(ball.ycor() + ball.dy)
+
+#Condições
+if points["p1"] == game_rules["win_points"]:
+    game_over = True
+    winner = "p1"
+elif points["p2"] == game_rules["win_points"]:
+    game_over = True
+    winner = "p2"
+
+#Colisão
+if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < hurdle_right.ycor() + 50 and ball.ycor() > hurdle_right.ycor() - 50):
+    ball.setx(340)
+    ball.dx*= -1
+elif (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < hurdle_left.ycor() + 50 and ball.ycor() > hurdle_left.ycor() - 50):
+    ball.goto(0, 0)
+    ball.dx *= -1
+    points["p2"] += 1
+    
+#Colisão da bola com topo 
+if ball.ycor() > 290:
+    ball.sety(290)
+    ball.dy *= -1
+elif ball.ycor() < -290:
+    ball.sety(-290)
+    ball.dy *= -1
+    
+#atualizar placar
+score_display.clear()
+score_display.write("p1: {}  p2: {}".format(points["p1"], points["p2"]), align="center", font=("Arial", 24, "normal"))
+
+
 
 #Mover a barreira (hurdle)
 
 
-
+#Apagar depois 
+#time.sleep(10)
 
 #Notas gerais 
 #Fundo - bgcolor("white")
